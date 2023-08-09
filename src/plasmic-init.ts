63b40d3890b8c0c420e738625f1be1e9cc7d0695
Jishnu Anandh <96691697+jishnu28@users.noreply.github.com>
2023-08-09 16:48:10 +0800

    Update plasmic-init.ts

--- src/plasmic-init.ts
+++ src/plasmic-init.ts
@@ -2,8 +2,8 @@ import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: "rPgURLJSo7wfHDtXZS9B9T",  // ID of a project you are using
-      token: "H7KS34yAA64rvvRdQ9byfI5brun0cLg79mSWOn9LVmgmz1EVltlnKBHUeY565JjXFk9iEiWscFACd2e5cSwnw"  // API token for that project
+      id: "vdVegr6NvnELgfB7Bzi3uo",  // ID of a project you are using
+      token: "RipVmbbHWGhwI6emamCnEjZ40jOpCcznY52wdPcei42iDfsWxyTNJnfCy7DALnIeyFkyzeuXA1OtflJqqg"  // API token for that project
     }
   ],
   // Fetches the latest revisions, whether or not they were unpublished!
