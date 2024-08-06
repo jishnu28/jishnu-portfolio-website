28f6a77680a5663f3fb373871536b4ff2b04b38d
Jishnu <e0725046@u.nus.edu>
2024-08-07 00:07:28 +0800

    Update plasmit-init-aft-NOC

--- src/plasmic-init.ts
+++ src/plasmic-init.ts
@@ -1,12 +1,13 @@
-import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+import { initPlasmicLoader } from '@plasmicapp/loader-nextjs';
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: "qDQMGpbhLZRyqmA1pt1z2M",  // ID of a project you are using
-      token: "K0L5ub1CfaqzkuDJVi30YRk7GV5Wy3vlgNP6NKqM1pNw0BB9k0eRBbLiTgaCmAZzPt7txeozRSI5uZAg"  // API token for that project
-    }
+      id: '87FT3dZPX1VsULQZcyHURa', // ID of a project you are using
+      token:
+        'TLvnp2eJ4Q8Qu6ZmPygyhgO3qo03rkwUKmEPmj2QHDVDEz80cEq8MV5I5hzLCn9VmoDKRj1Rx4zg0DAvIpn1Tw', // API token for that project
+    },
   ],
   // Fetches the latest revisions, whether or not they were unpublished!
   // Disable for production to ensure you render only published changes.
   preview: true,
-})
+});
